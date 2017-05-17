ed92fc2648a7000ab4b554de6da0e09f5738826b
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-17 16:17:00 -0700

    Write dfs topological sort

---
+++ lib/topological_sort.rb
@@ -0,0 +1,14 @@
+def dfs_topological_sort(vertices, seen = Set.new, sorted = [])
+  return sorted if vertices.empty?
+  vertices.each do |vertex|
+    seen.include?(vertex) ? next : seen.add(vertex)
+    neighbors = []
+    vertex.out_edges.each do |edge|
+      neighbors << edge.to_vertex unless seen.include?(edge.to_vertex)
+    end
+    dfs_topological_sort(neighbors, seen, sorted)
+    sorted.unshift(vertex)
+  end
+
+  sorted
+end
